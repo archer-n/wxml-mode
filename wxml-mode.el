6feef5a58bc3fe850c729471bf09782e7b2d076a
archer <lazyarcher.wang@gmail.com>
2022-02-07 11:42:35 +0800

    Tag attributes completion

--- wxml-mode.el
+++ wxml-mode.el
@@ -11,9 +11,9 @@
 ;; keymap
 (defvar wxml-mode-map
   (let ((map (make-keymap)))	;`sparse' doesn't allow binding to charsets.
-    (define-key map ">" 'nxml-balanced-close-start-tag-inline)
+    (define-key map (kbd ">") 'nxml-balanced-close-start-tag-inline)
     (define-key map (kbd "=") (lambda  () (interactive)
-                                (if (wxml-capf-grab-attribute)
+                                (if (length> (wxml-get-property 'attr-name (wxml-capf-grab-attribute)) 0)
                                     (progn
                                       (insert-char ?=)
                                       (insert-char ?\" 2)
@@ -51,8 +51,72 @@
 
 ;; completion
 (defvar wxml-data-file (expand-file-name "wxml-data.json" (file-name-directory load-file-name)))
-(defconst wxml-data (let ((json-object-type 'alist))
+(defconst wxml-data (let ((json-object-type 'plist))
                       (json-read-file wxml-data-file)))
+
+(defun wxml-data-find-global-attributes-to-completion-table ()
+  "Convert global properties into the completion list"
+ (let ((globalAttributes (plist-get wxml-data :globalAttributes)))
+  (if (vectorp globalAttributes)
+      (mapcar (lambda (globalAttribute)
+                (let ((name (plist-get globalAttribute :name))
+                      (description (plist-get globalAttribute :description))
+                      (annotation (plist-get globalAttribute :attrType)))
+                  (wxml-put-property name 'annotation annotation 'signature description)))
+              globalAttributes))))
+
+(defun wxml-data-find-tags ()
+  "Get all the tags"
+  (plist-get wxml-data :tags))
+
+(defun wxml-data-find-tags-to-completion-table ()
+  "Convert all the tags into the completion list"
+  (let ((tags (wxml-data-find-tags)))
+    (if (vectorp tags)
+        (mapcar (lambda (tag)
+                (let ((name (plist-get tag :name))
+                      (description (plist-get tag :description)))
+                  (wxml-put-property name 'signature description)))
+              tags))))
+
+
+(defun wxml-data-find-tag (name)
+  "Find tag by name"
+  (seq-find (lambda (tag)
+            (when (equal (plist-get tag :name) name)
+              tag))
+          (wxml-data-find-tags)))
+
+(defun wxml-data-find-tag-attributes (tag-name)
+  "Find a list of attributes for the tag"
+  (let ((tag (wxml-data-find-tag tag-name)))
+    (when (plist-member tag :attributes)
+      (plist-get tag :attributes))))
+
+(defun wxml-data-find-tag-attributes-to-completion-table (tag-name)
+  (let ((attributes (wxml-data-find-tag-attributes tag-name)))
+    (when (vectorp attributes)
+      (mapcar (lambda (attribute)
+                (let ((name (plist-get attribute :name))
+                      (description (plist-get attribute :description))
+                      (annotation (plist-get attribute :attrType)))
+                  (wxml-put-property name 'annotation annotation 'signature description)))
+              attributes))))
+
+(defun wxml-data-find-tag-attribute (tag attr)
+  "Find the tag attribute"
+  (let ((attributes (wxml-data-find-tag-attributes tag)))
+    (when (vectorp attributes)
+      (seq-find (lambda (attrbute)
+                  (if (equal (plist-get attrbute :name) attr)
+                      attrbute))
+                attributes))))
+
+(defun wxml-data-find-tag-attribute-values (tag attr)
+  "Find a list tag attribute values"
+  (let ((attribute (wxml-data-find-tag-attribute tag attr)))
+    (plist-get attribute :values)))
+
 
 (defun wxml-completion-at-point ()
   (let ((tag (wxml-capf-grab-tag))
@@ -65,7 +129,9 @@
    "Generate annotation for STR.
 STR is a candidate in a capf session.  See the implementation of
 `wxml-completion-at-point'."
-   (wxml-get-property 'annotation str))
+   (let ((annotation (wxml-get-property 'annotation str)))
+     (if (stringp annotation)
+   (format "(%s)" (wxml-get-property 'annotation str)))))
 
 (defun wxml-cap--get-docsig (str)
      "Generate docsig for STR.
@@ -79,9 +145,6 @@ STR is a candidate in a capf session.  See the implementation of
   (let ((symbol (company-grab-line "<[[:space:]]*\\([[:alnum:]_-]\\)*")))
     (when (stringp symbol)
       (substring symbol 1))))
-
-(defun wxml-completion-tag-name ()
-  (wxml-completion-tags (cdr (assq 'tags wxml-data))))
 
 (defun wxml-completion-tags (wxml-tags)
   (if (vectorp wxml-tags)
@@ -98,40 +161,36 @@ STR is a candidate in a capf session.  See the implementation of
                                     end
                                     (completion-table-dynamic
                                      (lambda (_)
-                                       (wxml-completion-tag-name)))
+                                       (wxml-data-find-tags-to-completion-table)))
                                     :annotation-function #'wxml-capf--get-annotation
                                     :company-docsig #'wxml-cap--get-docsig)))
 
 ;; attr
 
 (defun wxml-capf-grab-attribute ()
-  (let ((symbol (company-grab-line "\\(?:<[[:alnum:]]+\\([[:space:]]+[[:alnum:]]+=\".*\"\\)*[[:space:]]+[[:alnum:]]+\\)")))
+  (let ((symbol (company-grab-line "\\(?:<[[:alnum:]]+\\([[:space:]]+[[:alnum:]]+=\".*\"\\)*[[:space:]]+[[:alnum:]]*\\)")))
     (when (stringp symbol)
       (let* ((list (split-string symbol))
              (tag-name (substring (car list) 1))
-             (attr-name (car (last list))))
-        (wxml-put-property attr-name 'tag tag-name)))))
+             (attr-name (if (length> list 1)
+                            (car (last list))
+                          "")))
+        (wxml-put-property tag-name 'attr-name attr-name)))))
 
 
-(defun wxml-completion-global-attr ()
-  (let ((global-attrs (cdr (assq 'globalAttributes wxml-data))))
-    (when global-attrs
-      (let ((list (append global-attrs nil)))
-        (mapcar (lambda (attr)
-                  (let ((name (cdr (assq 'name attr)))
-                        (annotation (cdr (assq 'attrType attr)))
-                        (signature (cdr (assq 'description attr))))
-                    (wxml-put-property name 'annotation annotation 'signature signature)))
-                list)))))
 
-(defun wxml-attr-completion-at-point (attr)
-  (let* ((end (point))
+(defun wxml-attr-completion-at-point (tag)
+  (let* ((attr (wxml-get-property 'attr-name tag))
+         (end (point))
          (beg (- end (length attr))))
                               (list beg
                                     end
                                     (completion-table-dynamic
                                      (lambda (_)
-                                       (wxml-completion-global-attr)))
+                                       (flatten-list
+                                        (list
+                                         (wxml-data-find-tag-attributes-to-completion-table tag)
+                                         (wxml-data-find-global-attributes-to-completion-table)))))
                                     :annotation-function #'wxml-capf--get-annotation
                                     :company-docsig #'wxml-cap--get-docsig)))
 ;; attr-value
